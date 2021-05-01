017df4ad27f538d485975f632f7c7b3ea5109494
Dario Charles <dariocharles@outlook.com>
2021-05-01 16:32:01 -0400

    I worked on the css, adding functions and identifying the each card using the data attribute

---
+++ css/hello.js
@@ -0,0 +1,48 @@
+// Extra for storage when checking data
+
+// Storing the .memory-card items using the DOM
+const cards = document.querySelectorAll(".memory-card");
+// Testing
+console.log(cards);
+
+// 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
+function flipCard() {
+  // in this context the 'this' keyword represents the elements that fired the event
+  // add or take off the 'flip' class - which has the CSS to flip 180deg
+  this.classList.add('flip');
+
+  // If hasFlippedCard is false, meaning the player has clicked the first time. Else it is true and the player is clicking second time.
+  if (!hasFlippedCard){
+    hasFlippedCard = true;
+    // 'this' is the element that fired the event. In this case it is the div.memory-card.flip
+    firstCard = this;
+    // Testing
+    console.log({hasFlippedCard, firstCard})
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+    // Testing
+    console.log({firstCard, secondCard});
+
+    // Check if the cards match
+    // Testing
+    console.log(firstCard.dataset.framework);
+    console.log(secondCard.dataset.framework);
+
+    // If it's a match, else it's not a match
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+    } 
+    // Testing
+    console.log("Function was executed correctly!");
+    
+  }
+}
+
+// For each individual card (forEach(card) attach (=>) an event listener that when it is clicked the flipCard function will happen
+cards.forEach(card => card.addEventListener("click", flipCard));
--- js/script.js
+++ js/script.js
@@ -1,137 +1,41 @@
-// jQuery goes in the head of each HTML page
+
+  // Start at 22:41!!!!
 
 
+const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
+function flipCard() {
+  this.classList.add('flip');
 
-var xhr = new XMLHttpRequest();
-var data;
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
 
-xhr.open("GET", "http://swapi.co/api/"); //Making a request  ("GET/POST/PUT/DELETE", "API URL")
-xhr.send(); //Sending the request
-
-
-xhr.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-    document.getElementById ("data").innerHTML = this.responseText;
-    data = JSON.parse(this.responseText);
-}
-};
-
-// setTimeout Function
-setTimeout(function(){
-console.log(data):
-}, 500);//Telling the console.log(data) to wait 500 milliseconds before executing so that the "onreadystatechange" has time to reach a ready state of 4
-
-
-
-
-// Using the Caalback Function
-// Callback Function - a callback is a function that's passed as a parameter to another function and executed inside that function.
-//  They get around problems of timeout and because only invoked when we want them to 
-
-function getData(cb) { //cb stads for callback
-var xhr = new XMLHttpRequest();
-var data;
-
-xhr.open("GET", "http://swapi.co/api/"); //Making a request  ("GET/POST/PUT/DELETE", "API URL")
-xhr.send(); //Sending the request
-
-
-xhr.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-  document.getElementById ("data").innerHTML = this.responseText;
-  cb = JSON.parse(this.responseText);
-}
-};
-}
-
-function printDataToConsole(data){
-console.log(data);
-}
-
-getData(printDataToConsole) ;
-
-
-// Eg
-<html>
-<body>
-<button onclick="writeToDocument('http://swapi.co/api/people')">People</button>
-<button onclick="writeToDocument('http://swapi.co/api/plants')">Plants</button>
-<button onclick="writeToDocument('http://swapi.co/api/species')">Species</button>
-<button onclick="writeToDocument('http://swapi.co/api/starships')">Starships</button>
-<button onclick="writeToDocument('http://swapi.co/api/vehicles')">Vehicles</button>
-<button onclick="writeToDocument('http://swapi.co/api/films')">Films</button>
-<div id="data"></div>
-</body>
-</html>
-
-
-
-function getData(url, cb){
-var xhr = new XMLHttpRequest();
-
-
-xhr.open("GET", url); 
-xhr.send(); //Sending the request
-
-
-xhr.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-    document.getElementById ("data").innerHTML = this.responseText;
-    data = JSON.parse(this.responseText);
-}
-};
-
-function getTableheaders(obj){
-var tableHeaders = [];
-Object.keys(obj).forEach(function(key) {
-   tableHeaders.push(`<td>${key}</td>`);
-});
-return `<tr>${tableHeaders}</tr>`;
-}
-
-function generatepaginationButtons(next, prev) {
-if (next && prev) {
-  return `<button onclick="writeToDocument('${prev}')>Previous</button>
-  <button onclick="writeToDocument"('${next}')">Next</button>`;
-} else if (next && !prev) {
-  return `<button onclick="writeToDocument('${next}')">Next</button>
-          <button onclick="writeToDocument('${prev}')">Previous</button>`;
-}
-}
-function writeToDocument(url) {
-var tableRows = [];
-var el = document.getElementById(data); //el stands for element
-el.innerHTML = ""; //clears it everytime button is clicked
-
-getData(url, function(data) {
-//    console.dir(data);//dir is directory - this shows object prperties 
-// rather then [object]?
-  if (data.next || data.previous) {
-      var pagination;
-      pagination = generatPaginationButtons(data.next, data.previous);
-
+    // THIS
+    // IS
+    // Not
+    // WORKING
+    // 22:41
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+      // it's a match!
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+    } else {
+      // not a match
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+      }, 1500);
+    }
   }
-  data = data.results;
-  var tableHeaders = getTableHeaders(data[0]);
-
-  data.forEach(function(item) {
-      // forEach()
-      var dataRow = [];
-
-      Object.keys(item).forEach(function(key) {
-          var rowData = item[key].toString();
-          var truncatedData = rowData.substring(0, 15);
-          dataRow.push(`<td>${truncatedData}</td>`);
-      });
-      tableRows.push(`<tr>${dataRow}</tr>`);
-  });
-
-  el.innerHTM = `<table>${tableHeaders}${tableRows}${pagination}</table>`.replace(/,/g, "") ;       
-});
 }
 
-// Pagination
-// Object.key
-// Tabular data and Truncated Data
+cards.forEach(card => card.addEventListener('click', flipCard));
